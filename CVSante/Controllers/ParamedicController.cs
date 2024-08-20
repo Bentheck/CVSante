c024387a4970e8e9a4e562ee8cac5e1370356ced
Bentheck <bentheck@hotmail.com>
2024-08-20 15:58:51 -0400

    tes

--- CVSante/Controllers/ParamedicController.cs
+++ CVSante/Controllers/ParamedicController.cs
@@ -14,6 +14,7 @@ using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using CVSante.Tools;
 using System.Globalization;
+using static Google.Cloud.RecaptchaEnterprise.V1.TransactionData.Types;
 
 namespace CVSante.Controllers
 {
@@ -74,6 +75,13 @@ namespace CVSante.Controllers
             GetCurrentUser();
             if (_currentUser == null)
             {
+                return NotFound();
+            }
+
+            var currentUser = _context.UserParamedics
+                    .FirstOrDefaultAsync(up => up.FkIdentityUser == _currentUser.ToString()).Result;
+            if (currentUser == null)
+                {
                 return NotFound();
             }
 
