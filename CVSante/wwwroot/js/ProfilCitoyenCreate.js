cd0bf5d487808f9f7029ada40510122d11c76190
Bentheck <bentheck@hotmail.com>
2024-08-27 10:24:08 -0400

    tes

--- CVSante/wwwroot/js/ProfilCitoyenCreate.js
+++ CVSante/wwwroot/js/ProfilCitoyenCreate.js
@@ -5,19 +5,6 @@ let allergyIndex = allergyCount;
 let handicapIndex = handicapCount;
 let medicationIndex = medicationCount;
 
-//Mask pour le numéro de téléphone//
-
-function applyMasks() {
-    Inputmask({ "mask": "999-999-9999" }).mask(document.querySelectorAll('.phone-mask'));
-    Inputmask({ "mask": "A9A 9A9" }).mask(document.querySelectorAll('.CP-mask'));
-    Inputmask({ "mask": "9[9][9]" + ' Kg' }).mask(document.querySelectorAll('.poid-mask'));
-    Inputmask({ "mask": "9.9[9]" + ' Mètres' }).mask(document.querySelectorAll('.taille-mask'));
-}
-
-// Apply masks to existing fields on page load
-document.addEventListener("DOMContentLoaded", function () {
-    applyMasks();
-});
 
 //Fonction gérant les onglets//
 function showSection(sectionId) {
--- CVSante/wwwroot/js/ProfilCitoyenEdit.js
+++ CVSante/wwwroot/js/ProfilCitoyenEdit.js
@@ -1,17 +1,10 @@
 ﻿const sections = ['userinfo', 'address', 'antecedent', 'allergy', 'handicap', 'medication'];
 let currentSectionIndex = 0;
 
-// Initialize telephone input mask
-function applyMasks() {
-    Inputmask({ "mask": "999-999-9999" }).mask(document.querySelectorAll('.phone-mask'));
-    Inputmask({ "mask": "A9A 9A9" }).mask(document.querySelectorAll('.CP-mask'));
-    Inputmask({ "mask": "9[9][9]" + ' Kg' }).mask(document.querySelectorAll('.poid-mask'));
-    Inputmask({ "mask": "9.9[9]" + ' Mètres' }).mask(document.querySelectorAll('.taille-mask'));
-}
-
-// Apply masks to existing fields on page load
 document.addEventListener("DOMContentLoaded", function () {
-    applyMasks();
+    handlePrimaryAddressCheckbox();
+    attachAllergyEventListeners();
+    attachMedicationEventListeners();
 });
 
 // Show specific section and update navigation
